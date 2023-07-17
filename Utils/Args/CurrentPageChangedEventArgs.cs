417306fcfd05663b15c8f04d3eb17264312cd86e
Nacho <kulujun@gmail.com>
2023-07-17 13:04:39 +0800

    解决翻页问题

---
+++ Utils/Args/CurrentPageChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShadowViewer.Utils.Args
+{
+    public class CurrentPageChangedEventArgs
+    {
+        public CurrentPageChangedMode Mode { get; }
+        public int NewValue { get; }
+        public int OldValue { get; }
+        public CurrentPageChangedEventArgs(CurrentPageChangedMode mode, int newValue, int oldValue)
+        {
+            Mode = mode;
+            NewValue = newValue;
+            OldValue = oldValue;
+        }
+    }
+}
