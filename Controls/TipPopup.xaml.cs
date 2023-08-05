5604ab57a65691ed40a2e4e07cd468bf23fabac0
Nacho <kulujun@gmail.com>
2023-08-05 21:32:30 +0800

    update

---
+++ Controls/TipPopup.xaml.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+ 
+namespace ShadowViewer.Controls
+{
+    /// <summary>
+    /// 消息提醒.
+    /// </summary>
+    public sealed partial class TipPopup : UserControl
+    {
+        /// <summary>
+        /// <see cref="Text"/>的依赖属性.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(TipPopup), new PropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TipPopup"/> class.
+        /// </summary>
+        public TipPopup() => InitializeComponent();
+
+        public double DisplaySeconds { get; set; } = 2;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TipPopup"/> class.
+        /// </summary>
+        /// <param name="text">要显示的文本.</param>
+        /// <param name="type">信息级别</param>
+        /// <param name="displaySeconds">显示的时间</param>
+        public TipPopup(string text, InfoBarSeverity type = InfoBarSeverity.Informational, double displaySeconds = 2)
+            : this()
+        {
+            Text = text;
+            DisplaySeconds = displaySeconds;
+            switch (type)
+            {
+                case InfoBarSeverity.Informational:
+                    InformationIcon.Visibility = Visibility.Visible;
+                    PopupRootGrid.Background =
+                        Application.Current.Resources["InfoBarInformationalSeverityBackgroundBrush"] as SolidColorBrush;
+                    break;
+                case InfoBarSeverity.Success:
+                    SuccessIcon.Visibility = Visibility.Visible;
+                    PopupRootGrid.Background =
+                        Application.Current.Resources["InfoBarSuccessSeverityBackgroundBrush"] as SolidColorBrush;
+                    break;
+                case InfoBarSeverity.Warning:
+                    WarningIcon.Visibility = Visibility.Visible;
+                    PopupRootGrid.Background =
+                        Application.Current.Resources["InfoBarWarningSeverityBackgroundBrush"] as SolidColorBrush;
+                    break;
+                case InfoBarSeverity.Error:
+                    ErrorIcon.Visibility = Visibility.Visible;
+                    PopupRootGrid.Background =
+                        Application.Current.Resources["InfoBarErrorSeverityBackgroundBrush"] as SolidColorBrush;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 显示文本.
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        
+    }
+}
