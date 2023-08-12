c34989b0a2b8b918c2bbad5a766778d2aaf5d094
Nacho <kulujun@gmail.com>
2023-08-12 14:04:31 +0800

    修正空白逻辑

---
+++ Converters/CollectionCountToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using CommunityToolkit.WinUI.UI.Converters;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.VisualBasic;
+
+namespace ShadowViewer.Converters;
+
+public class CollectionToVisibilityConverter: EmptyObjectToObjectConverter
+{
+    protected override bool CheckValueIsEmpty(object value)
+    {
+        var isEmpty = true;
+        if (value is ICollection collection)
+        {
+            isEmpty = collection.Count == 0;
+            Log.Information("{r}11",isEmpty);
+        }
+        return isEmpty;
+    }
+}
