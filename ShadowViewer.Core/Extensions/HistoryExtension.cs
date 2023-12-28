61d189e02a7ba2e06c560f7ccd2a99dd8b2c2f48
Nacho <kulujun@gmail.com>
2023-12-28 20:51:17 +0800

    修复fluent icon

--- ShadowViewer.Core/Extensions/HistoryExtension.cs
+++ ShadowViewer.Core/Extensions/HistoryExtension.cs
@@ -2,13 +2,16 @@
 
 namespace ShadowViewer.Extensions;
 
+/// <summary>
+/// 历史记录拓展类,通过时间比较
+/// </summary>
 public class HistoryExtension : IComparer<IHistory>
 {
 
     private readonly CaseInsensitiveComparer caseiComp = new();
 
-    public int Compare(IHistory x, IHistory y)
+    public int Compare(IHistory? x, IHistory? y)
     {
-        return caseiComp.Compare( y.Time,x.Time);
+        return caseiComp.Compare( y?.Time,x?.Time);
     }
 }
