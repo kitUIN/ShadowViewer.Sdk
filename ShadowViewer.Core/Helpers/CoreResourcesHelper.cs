9588e79ff5824a4a30aa05b3bd71ddb5f2eb8ce8
Nacho <kulujun@gmail.com>
2023-12-29 15:58:35 +0800

    调整Resource

--- ShadowViewer.Core/Helpers/CoreResourcesHelper.cs
+++ ShadowViewer.Core/Helpers/CoreResourcesHelper.cs
@@ -2,15 +2,14 @@
 {
     internal static class CoreResourcesHelper
     {
-        private static readonly ResourceManager resourceManager = new ResourceManager();
         private static readonly string prefix = "ShadowViewer.Core/Resources/";
         public static string GetString(string key)
         {
-            return resourceManager.MainResourceMap.GetValue(prefix + key).ValueAsString;
+            return ResourcesHelper.GetString(key,prefix);
         }
         public static string GetString(CoreResourceKey key)
         {
-            return GetString(key.ToString());
+            return ResourcesHelper.GetString(key, prefix);
         }
     }
 }
