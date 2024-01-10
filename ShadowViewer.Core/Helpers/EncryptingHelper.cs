a8e078602c91693befd33a08b5e00563ad5430b4
Nacho <kulujun@gmail.com>
2024-01-11 03:54:19 +0800

    简化 EncryptingHelper

--- ShadowViewer.Core/Helpers/EncryptingHelper.cs
+++ ShadowViewer.Core/Helpers/EncryptingHelper.cs
@@ -2,62 +2,58 @@
 
 namespace ShadowViewer.Helpers
 {
+    /// <summary>
+    /// 加密工具类
+    /// </summary>
     public static class EncryptingHelper
     {
-        public static string CreateSha1(string path)
+        /// <summary>
+        /// 同时加密MD5与Sha1
+        /// </summary> 
+        /// <returns>MD5,SHA1</returns>
+        public static (string, string) CreateMd5AndSha1(string path)
         {
-            using (SHA1 sha1 = SHA1.Create())
-            {
-                using (FileStream stream = File.OpenRead(path))
-                {
-                    byte[] hash = sha1.ComputeHash(stream);
-                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-                }
-            }
+            using FileStream stream = File.OpenRead(path);
+            var sha1Hash = CreateSha1(stream);
+            stream.Seek(0, SeekOrigin.Begin);
+            var md5Hash = CreateMd5(stream);
+            return (md5Hash, sha1Hash);
         }
+        public static string CreateSha1(string path)
+        { 
+            using FileStream stream = File.OpenRead(path);
+            return CreateSha1(stream);
+        }
+        
         public static string CreateMd5(string path)
         {
-            using (MD5 md5 = MD5.Create())
-            {
-                using (FileStream stream = File.OpenRead(path))
-                {
-                    byte[] hash = md5.ComputeHash(stream);
-                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-                }
-            }
+            using FileStream stream = File.OpenRead(path);
+            return CreateMd5(stream);
         }
         public static string CreateSha1(Stream stream)
         {
-            using (SHA1 sha1 = SHA1.Create())
-            {
-                byte[] hash = sha1.ComputeHash(stream);
-                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-            }
+            using SHA1 sha1 = SHA1.Create();
+            return Bytes2String(sha1.ComputeHash(stream));
+        }
+        
+        public static string CreateMd5(Stream stream)
+        {
+            using MD5 md5 = MD5.Create();
+            return Bytes2String(md5.ComputeHash(stream));
+        }
+        public static string CreateMd5(byte[] bytes)
+        { 
+            using MD5 md5 = MD5.Create();
+            return Bytes2String(md5.ComputeHash(bytes));
         }
         public static string CreateSha1(byte[] bytes)
         {
-            using (SHA1 sha1 = SHA1.Create())
-            {
-                byte[] hash = sha1.ComputeHash(bytes);
-                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-            }
+            using var sha1 = SHA1.Create();
+            return Bytes2String(sha1.ComputeHash(bytes));
         }
-        public static string CreateMd5(Stream stream)
+        public static string Bytes2String(byte[] bytes)
         {
-
-            using (MD5 md5 = MD5.Create())
-            {
-                byte[] hash = md5.ComputeHash(stream);
-                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-            }
-        }public static string CreateMd5(byte[] bytes)
-        {
-
-            using (MD5 md5 = MD5.Create())
-            {
-                byte[] hash = md5.ComputeHash(bytes);
-                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-            }
+            return BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
         }
     }
 }
