549c2d8f0bc4eeb509c8ed6d7fb1b35e40eb51eb
kitUIN <Kit_UIN@outlook.com>
2025-12-15 14:38:41 +0800

    Fix | FirstUri

--- ShadowViewer.Sdk/Helpers/ResponderHelper.cs
+++ ShadowViewer.Sdk/Helpers/ResponderHelper.cs
@@ -6,6 +6,7 @@ using ShadowPluginLoader.WinUI;
 using ShadowViewer.Sdk.Responders;
 
 namespace ShadowViewer.Sdk.Helpers;
+
 /// <summary>
 /// 响应服务帮助类
 /// </summary>
@@ -27,6 +28,16 @@ public class ResponderHelper
         var pluginLoader = DiFactory.Services.Resolve<PluginLoader>();
         return GetResponders<TResponder>().Where(x => pluginLoader.IsEnabled(x.Id) == true);
     }
+
+    /// <summary>
+    /// 获取启用的指定Id的INavigationResponder
+    /// </summary>
+    public static INavigationResponder? GetEnabledNavigateResponder(string id)
+    {
+        var realId = "ShadowViewer.Plugin." + id;
+        return GetEnabledResponder<INavigationResponder>(realId);
+    }
+
     /// <summary>
     /// 获取启用的指定Id的响应类
     /// </summary>
@@ -34,17 +45,20 @@ public class ResponderHelper
     {
         var pluginLoader = DiFactory.Services.Resolve<PluginLoader>();
         if (GetResponders<TResponder>()
-            .FirstOrDefault(x => string.Equals(id, x.Id, StringComparison.CurrentCultureIgnoreCase)) is { } responder &&
-            pluginLoader.IsEnabled(id) == true) return responder;
+                    .FirstOrDefault(x => string.Equals(id, x.Id, StringComparison.CurrentCultureIgnoreCase)) is
+                { } responder &&
+            pluginLoader.IsEnabled(responder.Id) == true) return responder;
         return default;
     }
+
     /// <summary>
     /// 获取指定Id的响应类
     /// </summary>
     public static TResponder? GetResponder<TResponder>(string id) where TResponder : IResponder
     {
         if (GetResponders<TResponder>()
-                .FirstOrDefault(x => string.Equals(id, x.Id, StringComparison.CurrentCultureIgnoreCase)) is { } responder) return responder;
+                .FirstOrDefault(x => string.Equals(id, x.Id, StringComparison.CurrentCultureIgnoreCase)) is
+            { } responder) return responder;
         return default;
     }
 }
